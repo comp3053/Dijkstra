0324d15a2f49d2e148e6beae79e0d2835b7533c9
ecwu <bitlabwzh@gmail.com>
2019-04-20 15:54:31 +0800

    change warning to throw exception

--- src/model/Equipment.java
+++ src/model/Equipment.java
@@ -4,34 +4,32 @@ public class Equipment {
     private String name;
     private int volume;
 
-    public Equipment(String name, int volume) {
-        this.name = name;
-        this.volume = volume;
-        // TODO: save data to the database
+    public Equipment(String name, int volume) throws InvalidEquipmentInputException, EmptyEquipmentNameException {
+        setName(name);
+        setVolume(volume);
     }
 
     public String getName() {
-        // TODO: get data from database
         return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-        // TODO save "name" to the database
+    public void setName(String name) throws EmptyEquipmentNameException {
+        if (name.isEmpty()){
+           throw new EmptyEquipmentNameException("Equipment name cannot be empty!");
+        }else {
+            this.name = name;
+        }
     }
 
     public int getVolume() {
-        // TODO: get data from database
-
         return this.volume;
-
     }
 
-    public void setVolume(int volume) {
-        if(volume >= 0)
+    public void setVolume(int volume) throws InvalidEquipmentInputException {
+        if (volume > 0) {
             this.volume = volume;
-        else
-            System.out.println("Invalid Input");
-        // TODO save "volume" to the database
+        } else {
+            throw new InvalidEquipmentInputException("Volume should be greater than 0!");
+        }
     }
 }
