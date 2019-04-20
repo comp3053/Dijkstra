51536d0f62417f59c68cd8f18d8b7a5a3a1bdb67
ecwu <bitlabwzh@gmail.com>
2019-04-20 15:54:43 +0800

    modify equipment test

--- src/test/EquipmentTest.java
+++ src/test/EquipmentTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import model.EmptyEquipmentNameException;
 import model.Equipment;
+import model.InvalidEquipmentInputException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,32 +10,62 @@ import static org.junit.Assert.*;
 
 public class EquipmentTest {
 
-    private static Equipment equipment = new Equipment("myEquipment",500);
+    private static Equipment equipment;
+
+    @Before
+    public void setUp() {
+        try {
+            equipment = new Equipment("myEquipment", 500);
+        } catch (InvalidEquipmentInputException | EmptyEquipmentNameException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void getName() {
-        assertEquals("myEquipment",equipment.getName());
+        assertEquals("myEquipment", equipment.getName());
     }
 
     @Test
     public void setName() {
-        equipment.setName("yourEquipment");
-        assertEquals("yourEquipment",equipment.getName());
+        try {
+            equipment.setName("yourEquipment");
+        } catch (EmptyEquipmentNameException e) {
+            e.printStackTrace();
+        }
+        assertEquals("yourEquipment", equipment.getName());
+        try {
+            equipment.setName("");
+        } catch (EmptyEquipmentNameException e) {
+            e.printStackTrace();
+        }
+        assertEquals("yourEquipment", equipment.getName());
     }
 
     @Test
     public void getVolume() {
-        assertEquals(500,equipment.getVolume());
+        assertEquals(500, equipment.getVolume());
     }
 
     @Test
     public void setVolume() {
-        equipment.setVolume(-200);
-        assertEquals(500,equipment.getVolume());
-        /*
-            TODO: once the volume is successfully set, all "getVolume()" method will return the value was set
-            Even though the "getVolume()" method is above "setVolume()" Try to fix that bugs.
-        */
-
+        try {
+            equipment.setVolume(1000);
+        } catch (InvalidEquipmentInputException e) {
+            e.printStackTrace();
+        }
+        assertEquals(1000, equipment.getVolume());
+        try {
+            equipment.setVolume(0);
+        } catch (InvalidEquipmentInputException e) {
+            e.printStackTrace();
+        }
+        assertEquals(1000, equipment.getVolume());
+        try {
+            equipment.setVolume(-100);
+        } catch (InvalidEquipmentInputException e) {
+            e.printStackTrace();
+        }
+        assertEquals(1000, equipment.getVolume());
     }
 }
