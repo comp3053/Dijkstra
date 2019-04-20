a0bd9d8c50caf8fd82ba11399842fae7b335d4d8
dengrenjie <dengrenjie31@gmail.com>
2019-04-20 21:04:56 +0800

    finish IngredientTest

--- src/test/IngredientTest.java
+++ src/test/IngredientTest.java
@@ -1,5 +1,9 @@
 package test;
 
+import model.EmptyIngredientNameException;
+import model.Ingredient;
+import model.InvalidIngredientAmountException;
+import utils.UnitEnum;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,39 +11,78 @@ import static org.junit.Assert.*;
 
 public class IngredientTest {
 
+    private static Ingredient ingredient;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        try {
+            ingredient = new Ingredient(1,"yeast", 100.0, UnitEnum.GRAM);
+        } catch (EmptyIngredientNameException | InvalidIngredientAmountException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void getID() {
+        assertEquals(1, ingredient.getID());
     }
 
     @Test
     public void setID() {
+        ingredient.setID(2);
+        assertEquals(2, ingredient.getID());
     }
 
     @Test
     public void getName() {
+        assertEquals("yeast", ingredient.getName());
     }
 
     @Test
     public void setName() {
+        try {
+            ingredient.setName("water");
+        } catch (EmptyIngredientNameException e) {
+            e.printStackTrace();
+        }
+        assertEquals("water", ingredient.getName());
+        try {
+            ingredient.setName("");
+        } catch (EmptyIngredientNameException e) {
+            e.printStackTrace();
+        }
+        assertEquals("water", ingredient.getName());
     }
 
     @Test
     public void getAmount() {
+        assertEquals(100.0, ingredient.getAmount(), 0.00);
     }
 
     @Test
     public void setAmount() {
+        try {
+            ingredient.setAmount(233.3);
+        } catch (InvalidIngredientAmountException e) {
+            e.printStackTrace();
+        }
+        assertEquals(233.3, ingredient.getAmount(), 0.00);
+        try {
+            ingredient.setAmount(-66.6);
+        } catch (InvalidIngredientAmountException e) {
+            e.printStackTrace();
+        }
+        assertEquals(233.3, ingredient.getAmount(), 0.00);
     }
 
     @Test
     public void getUnit() {
+        assertEquals(UnitEnum.GRAM, ingredient.getUnit());
     }
 
     @Test
     public void setUnit() {
+        ingredient.setUnit(UnitEnum.KILOGRAM);
+        assertEquals(UnitEnum.KILOGRAM, ingredient.getUnit());
     }
 }
