767d109714b4516631d88bf08c704fa71c826ce2
ecwu <bitlabwzh@gmail.com>
2019-04-20 16:55:16 +0800

    Add Test classes for two ingredient classes:
    - Ingredient Class
    - StorageIngredient Class

---
+++ src/test/IngredientTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getID() {
+    }
+
+    @Test
+    public void setID() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getAmount() {
+    }
+
+    @Test
+    public void setAmount() {
+    }
+
+    @Test
+    public void getUnit() {
+    }
+
+    @Test
+    public void setUnit() {
+    }
+}
---
+++ src/test/StorageIngredientTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StorageIngredientTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addAmount() {
+    }
+}
