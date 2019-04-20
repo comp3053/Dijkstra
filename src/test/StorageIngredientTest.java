be99cd91493c6015d81321b8f2973b6b493dbd18
dengrenjie <dengrenjie31@gmail.com>
2019-04-20 21:15:11 +0800

    finish StorageIngredient test

--- src/test/StorageIngredientTest.java
+++ src/test/StorageIngredientTest.java
@@ -1,17 +1,41 @@
 package test;
 
+import model.EmptyIngredientNameException;
+import model.InvalidIngredientAmountException;
+import model.StorageIngredient;
 import org.junit.Before;
 import org.junit.Test;
+import utils.UnitEnum;
 
 import static org.junit.Assert.*;
 
 public class StorageIngredientTest {
 
+    private static StorageIngredient storageIngredient;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        try {
+            storageIngredient = new StorageIngredient(1, "wheat", 5.0, UnitEnum.KILOGRAM);
+        } catch (EmptyIngredientNameException | InvalidIngredientAmountException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void addAmount() {
+        try {
+            storageIngredient.addAmount(5.0);
+        } catch (InvalidIngredientAmountException e) {
+            e.printStackTrace();
+        }
+        assertEquals(10.0, storageIngredient.getAmount(), 0.00);
+
+        try {
+            storageIngredient.addAmount(-10.0);
+        } catch (InvalidIngredientAmountException e) {
+            e.printStackTrace();
+        }
+        assertEquals(10.0, storageIngredient.getAmount(), 0.00);
     }
 }
