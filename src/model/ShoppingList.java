babb066b14b58cc38326ec67e74b854895bcdd71
Richard <694834997@qq.com>
2019-04-27 15:20:48 +0800

    correct some error in the Note class

--- src/model/ShoppingList.java
+++ src/model/ShoppingList.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class ShoppingList {
     private ArrayList<Ingredient> missingIngredients;
 
+    public ShoppingList(ArrayList<Ingredient> missingIngredients){
+        this.missingIngredients=missingIngredients;
+    }
+
     public double calculateMissingIngredientAmount(double restAmount, double requireAmount){
         return (requireAmount-restAmount);
     }
