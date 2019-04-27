3cd70d31e2d8b1397f584f49bbefc83d3b50c7a1
LittleS <qqmsnco@126.com>
2019-04-27 16:05:00 +0800

    Start.java: modify

--- src/Starter.java
+++ src/Starter.java
@@ -1,11 +1,12 @@
 import view.HomeView;
+import view.UpdateEquipmentInfoView;
 
 public class Starter {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() { // run that code on the event dispatch thread
             @Override
             public void run() {
-                HomeView hv = new HomeView();
+                UpdateEquipmentInfoView hv = new UpdateEquipmentInfoView();
                 hv.setVisible(true);
             }
         });
