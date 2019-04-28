7f5f421d735d23413f6af3ce214b2f4e72255c29
LittleS <qqmsnco@126.com>
2019-04-28 21:22:19 +0800

    BrewingHistoryListView.java: now the UI is a little bit more beautiful

--- src/view/BrewingHistoryListView.java
+++ src/view/BrewingHistoryListView.java
@@ -1,7 +1,6 @@
 package view;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class BrewingHistoryListView extends View {
@@ -11,26 +10,30 @@ public class BrewingHistoryListView extends View {
         this.setLayout(new BorderLayout());
 
         JPanel jp_header = new JPanel();
-        jp_header.setLayout(new FlowLayout());
+        jp_header.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         /* back button*/
         JButton btn_back = new JButton("back");
         jp_header.add(btn_back);
 
         JLabel msg_header = new JLabel("Add note - select brew history");
+        msg_header.setFont(new Font(msg_header.getFont().getFontName(), msg_header.getFont().getStyle(), 24));
         jp_header.add(msg_header);
 
-        this.add(jp_header, BorderLayout.PAGE_START);
+        this.add(jp_header, BorderLayout.NORTH);
 
         JPanel jp_main = new JPanel();
-        jp_main.setLayout(new FlowLayout());
+        jp_main.setLayout(new BoxLayout(jp_main,BoxLayout.Y_AXIS));
         for(int i =0;i<5;i++) {
-            JLabel msg_historyi = new JLabel("brew history" + "03");
-            jp_main.add(msg_historyi);
-            JLabel msg_time = new JLabel("2019-04-28");
-            jp_main.add(msg_time);
+            JPanel jp_main_i = new JPanel();
+            jp_main_i.setLayout(new FlowLayout());
+            JLabel msg_historyi = new JLabel("brew history" + " 03 ");
+            jp_main_i.add(msg_historyi);
+            JLabel msg_time = new JLabel("2019-04-28 ");
+            jp_main_i.add(msg_time);
             JButton btn_takeNote = new JButton("Take note");
-            jp_main.add(btn_takeNote);
+            jp_main_i.add(btn_takeNote);
+            jp_main.add(jp_main_i);
         }
         this.add(jp_main, BorderLayout.CENTER);
     }
