355570aca2f512c8b01b488e88073efd7adcdb77
LittleS <qqmsnco@126.com>
2019-04-28 21:14:47 +0800

    NoteInputView.java: now the UI is a little bit more beautiful

--- src/view/NoteInputView.java
+++ src/view/NoteInputView.java
@@ -11,31 +11,33 @@ public class NoteInputView extends View{
         this.setLayout(new BorderLayout());
 
         JPanel jp_header = new JPanel();
-        jp_header.setLayout(new FlowLayout());
+        jp_header.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         /* back button*/
         JButton btn_back = new JButton("back to note list");
-        btn_back.setBounds(50,50,100,50);
         jp_header.add(btn_back);
 
-        JLabel msg_header = new JLabel("Add note");
+        /* This part change the design */
+        JLabel msg_header = new JLabel("Add note for brewing history" + "01");
+        msg_header.setFont(new Font(msg_header.getFont().getFontName(), msg_header.getFont().getStyle(), 24));
         jp_header.add(msg_header);
 
-        this.add(jp_header, BorderLayout.PAGE_START);
+        this.add(jp_header, BorderLayout.NORTH);
 
         JPanel jp_main = new JPanel();
-        jp_main.setLayout(new FlowLayout());
-        JLabel msg_noteInfo = new JLabel("Tasting note for brewing history" + "01");
-        jp_main.add(msg_noteInfo);
-        JTextField input_noteContent = new JTextField("Please write down your note");
-        jp_main.add(input_noteContent);
+        jp_main.setLayout(new BorderLayout());
+        jp_main.setBorder(new EmptyBorder(0,20,10,20)); // top and left padding for recommend entry
+
+        JTextArea input_noteContent = new JTextArea("Please write down your note here");
+        jp_main.add(input_noteContent,BorderLayout.CENTER);
 
         this.add(jp_main, BorderLayout.CENTER);
 
         JPanel jp_foot = new JPanel();
+        jp_foot.setLayout(new FlowLayout(FlowLayout.RIGHT));
         JButton btn_save = new JButton("save");
         jp_foot.add(btn_save);
-        this.add(jp_foot, BorderLayout.LINE_END);
+        this.add(jp_foot, BorderLayout.SOUTH);
 
 
     }
