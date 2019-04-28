0bc35341aa3b268e44f5c2163d4aa1e243ce5a85
LittleS <qqmsnco@126.com>
2019-04-28 20:52:46 +0800

    EquipmentInforView.java: now the UI is a little bit more beautiful

--- src/view/EquipmentInfoView.java
+++ src/view/EquipmentInfoView.java
@@ -1,33 +1,32 @@
 package view;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class EquipmentInfoView extends View{
     public EquipmentInfoView(){
         this.setTitle("Equipment Information"); // set frame title
-        this.setSize(800, 600); // set frame size
+        this.setSize(600, 200); // set frame size
         this.setLayout(new BorderLayout());
 
         JPanel jp_header = new JPanel();
-        jp_header.setLayout(new FlowLayout());
+        jp_header.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         /* back button*/
         JButton btn_back = new JButton("back");
-        btn_back.setBounds(50,50,100,50);
         jp_header.add(btn_back);
+        JLabel msg_header = new JLabel("Equipment Information");
+        msg_header.setFont(new Font(msg_header.getFont().getFontName(), msg_header.getFont().getStyle(), 24));
+        jp_header.add(msg_header);
 
-
-        this.add(jp_header, BorderLayout.PAGE_START);
+        this.add(jp_header, BorderLayout.NORTH);
 
         JPanel jp_main = new JPanel();
-        jp_main.setLayout(new FlowLayout());
+        jp_main.setLayout(new BoxLayout(jp_main, BoxLayout.Y_AXIS));
         JLabel msg_model = new JLabel("Model" + "HOMEBREW-2018");
         JLabel msg_material = new JLabel("Model" + "HOMEBREW-2018");
         JLabel msg_volume = new JLabel("Model" + "HOMEBREW-2018");
         JLabel msg_purchase_date = new JLabel("Model" + "HOMEBREW-2018");
-        msg_model.setFont(new Font(msg_model.getFont().getFontName(), msg_model.getFont().getStyle(), 24));
         jp_main.add(msg_model);
         jp_main.add(msg_material);
         jp_main.add(msg_volume);
@@ -35,9 +34,10 @@ public class EquipmentInfoView extends View{
         this.add(jp_main, BorderLayout.CENTER);
 
         JPanel jp_foot = new JPanel();
+        jp_foot.setLayout(new FlowLayout(FlowLayout.RIGHT));
         JButton btn_update = new JButton("update");
         jp_foot.add(btn_update);
-        this.add(jp_foot, BorderLayout.LINE_END);
+        this.add(jp_foot, BorderLayout.SOUTH);
     }
 
     @Override
