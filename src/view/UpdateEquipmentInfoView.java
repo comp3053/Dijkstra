dbf5a04125cc1f41edb9bb5243634202d4776e8c
LittleS <qqmsnco@126.com>
2019-04-28 20:43:19 +0800

    UpdateEquipmentInforView.java: now the UI is a little bit more beautiful

--- src/view/UpdateEquipmentInfoView.java
+++ src/view/UpdateEquipmentInfoView.java
@@ -7,26 +7,25 @@ import java.awt.*;
 public class UpdateEquipmentInfoView extends View{
     public UpdateEquipmentInfoView(){
         this.setTitle("Equipment Update"); // set frame title
-        this.setSize(800, 600); // set frame size
+        this.setSize(600, 200); // set frame size
         this.setLayout(new BorderLayout());
 
         JPanel jp_header = new JPanel();
-        jp_header.setLayout(new FlowLayout());
+        jp_header.setLayout(new FlowLayout(FlowLayout.LEFT));
 
         /* back button*/
         JButton btn_back = new JButton("back");
-        btn_back.setBounds(50,50,100,50);
         jp_header.add(btn_back);
 
         JLabel msg_header = new JLabel("Update Equipment Information");
         msg_header.setFont(new Font(msg_header.getFont().getFontName(), msg_header.getFont().getStyle(), 24));
-        msg_header.setBounds(100,50,100,50);
         jp_header.add(msg_header);
-        this.add(jp_header, BorderLayout.PAGE_START);
+        this.add(jp_header, BorderLayout.NORTH);
 
 
         JPanel jp_main = new JPanel();
-        jp_main.setLayout(new FlowLayout());
+        jp_main.setLayout(new BoxLayout(jp_main, BoxLayout.Y_AXIS));// Vertically display
+        jp_main.setBorder(new EmptyBorder(0,0,350,0));
         JTextField textfield_Model = new JTextField("Model");
         JTextField textfiled_Material = new JTextField("Material");
         JTextField textfield_Volumn = new JTextField("Volume");
@@ -39,9 +38,11 @@ public class UpdateEquipmentInfoView extends View{
         this.add(jp_main, BorderLayout.CENTER);
 
         JPanel jp_foot = new JPanel();
+        jp_foot.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        jp_foot.setBorder(new EmptyBorder(0,0,0,0));
         JButton btn_save = new JButton("save");
         jp_foot.add(btn_save);
-        this.add(jp_foot, BorderLayout.LINE_END);
+        this.add(jp_foot, BorderLayout.SOUTH);
     }
 
     @Override
