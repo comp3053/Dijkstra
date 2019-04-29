d94d2e6c2a783082023e032059a5e2c1b443c6ea
dengrenjie <dengrenjie31@gmail.com>
2019-04-29 09:31:48 +0800

    Finish Equipment database part

--- src/controller/EquipmentController.java
+++ src/controller/EquipmentController.java
@@ -23,20 +23,61 @@ public class EquipmentController {
             ResultSet rs = dbHelper.execSqlWithReturn("SELECT * FROM Equipment");
             name = rs.getString(2);
             volume = rs.getInt(3);
+            dbHelper.closeConnection();
         } catch (SQLException | SQLiteConnectionException e) {
             e.printStackTrace();
             throw new FetchDataException("Could not fetch Equipment information.");
         }
         return new Equipment(name, volume);
+    }
 
+    private boolean insertEquipment(Equipment equipment) {
+        DatabaseHelper dbHelper = new DatabaseHelper();
+        dbHelper.connectSQLite();
+        String query = String.format("INSERT INTO Equipment VALUES (1,'%s',%d)",
+                equipment.getName(), equipment.getVolume());
+        try {
+            dbHelper.execSqlNoReturn(query);
+            dbHelper.closeConnection();
+        } catch (SQLiteConnectionException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean setEquipment(Equipment equipment) {
+        DatabaseHelper dbHelper = new DatabaseHelper();
+        EquipmentController ec = new EquipmentController();
+        dbHelper.connectSQLite();
+        String query = String.format("UPDATE Equipment SET name='%s',volume=%d WHERE Equipment_ID=1",
+                equipment.getName(), equipment.getVolume());
+        try {
+            ec.getEquipment();
+            dbHelper.closeConnection();
+        } catch (InvalidEquipmentVolumeException | EmptyEquipmentNameException e) {
+            e.printStackTrace();
+            return false;
+        } catch (FetchDataException e) {
+            insertEquipment(equipment);
+        }
+        dbHelper.connectSQLite();
+        try {
+            dbHelper.execSqlNoReturn(query);
+            dbHelper.closeConnection();
+        } catch (SQLiteConnectionException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         EquipmentController ec = new EquipmentController();
         try {
-            Equipment equipment = ec.getEquipment();
-            System.out.println(equipment.getName());
-            System.out.println(equipment.getVolume());
+            Equipment equipment = new Equipment("MyEquipment", 2000);
+            ec.setEquipment(equipment);
+            System.out.println(ec.getEquipment().getName());
         } catch (FetchDataException | InvalidEquipmentVolumeException | EmptyEquipmentNameException e) {
             e.printStackTrace();
         }
