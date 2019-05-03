74064ef03d0e83ccbd4dbcdb8b425be1e2bf19cf
ecwu <bitlabwzh@gmail.com>
2019-05-04 00:13:26 +0800

    remove TODO

--- src/Starter.java
+++ src/Starter.java
@@ -10,95 +10,76 @@ public class Starter {
 //                HomeController hc = new HomeController();
 //                HomeView hv = new HomeView(hc);
 //                hv.setVisible(true);
-                // FIXME: [v] Changed Button
 //
 //                RecipeView rv = new RecipeView();
 //                rv.setVisible(true);
-                // FIXME: [v] Changed Button
 //
 //                RecipeFormView rfv = new RecipeFormView();
 //                rfv.setVisible(true);
-                // FIXME: [] Changed Button
 //
 //                UpdateEquipmentInforController ueic = new UpdateEquipmentInforController();
 //                UpdateEquipmentInfoView ueiv = new UpdateEquipmentInfoView(ueic);
 //                ueiv.setVisible(true);
-                // FIXME: [] Changed Button
 //
 //                EquipmentInfoController eic = new EquipmentInfoController();
 //                EquipmentInfoView ei = new EquipmentInfoView(eic);
 //                ei.setVisible(true);
-                // FIXME: [] Changed Button
 //
 //                NoteInputController nic = new NoteInputController();
 //                NoteInputView niv = new NoteInputView(nic, 3);
 //                niv.setVisible(true);
-                // FIXME: [] Changed Button
 //
 //                BrewingHistoryListController bhlc = new BrewingHistoryListController();
 //                BrewingHistoryListView bhlv = new BrewingHistoryListView(bhlc);
 //                bhlv.setVisible(true);
-                // FIXME: [v] Changed Button
 
 //
-//                NoteContentController ncc = new NoteContentController();
-//                NoteContentView ncv = new NoteContentView(ncc, 1);
-//                ncv.setVisible(true);
-                // FIXME: [v] Changed Button
+                NoteContentController ncc = new NoteContentController();
+                NoteContentView ncv = new NoteContentView(ncc, 1);
+                ncv.setVisible(true);
 
 //
 //                NoteListController nlc = new NoteListController();
 //                NoteListView nlv = new NoteListView(nlc);
 //                nlv.setVisible(true);
-                // FIXME: [v] Changed Button
 
 //
 //                MissingIngredientListController milc = new MissingIngredientListController();
 //                MissingIngredientsListView milv = new MissingIngredientsListView(milc);
 //                milv.setVisible(true);
-                // FIXME: [] Changed Button
 
-//
-//                RecommendNotEnoughIngredientRecipeListController rilc = new RecommendNotEnoughIngredientRecipeListController();
-//                RecommendNotEnoughIngredientRecipeListView rilv = new RecommendNotEnoughIngredientRecipeListView(rilc);
-//                rilv.setVisible(true);
-                // FIXME: [] Changed Button
 
-//
-//                RecommendRecipeListView rrlv = new RecommendRecipeListView();
-//                rrlv.setVisible(true);
-                // FIXME: [] Changed Button
+                RecommendNotEnoughIngredientRecipeListController rilc = new RecommendNotEnoughIngredientRecipeListController();
+                RecommendNotEnoughIngredientRecipeListView rilv = new RecommendNotEnoughIngredientRecipeListView(rilc);
+                rilv.setVisible(true);
 
-//
+                RecommendRecipeListController rrlc = new RecommendRecipeListController();
+                RecommendRecipeListView rrlv = new RecommendRecipeListView(rrlc);
+                rrlv.setVisible(true);
+
 //
 //                RecipeDetailView rdv = new RecipeDetailView();
 //                rdv.setVisible(true);
-                // FIXME: [v] Changed Button
 
 //
 //                IngredientListView ilv = new IngredientListView();
 //                ilv.setVisible(true);
-                // FIXME: [v] Changed Button
 
 //
 //                IngredientFormView ifv = new IngredientFormView();
 //                ifv.setVisible(true);
-                // FIXME: [] Changed Button
 
 //
 //                IngredientDetailView idv = new IngredientDetailView();
 //                idv.setVisible(true);
-                // FIXME: [v] Changed Button
 
 //
 //                BrewDetailView bdv = new BrewDetailView();
 //                bdv.setVisible(true);
-                // FIXME: [] Changed Button
 
 //
 //                BrewView bv = new BrewView();
 //                bv.setVisible(true);
-                // FIXME: [] Changed Button
 
 //
 //                LayoutTestView ltv = new LayoutTestView();
