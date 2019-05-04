a34d26a4ed290ed801e8102753c164c5d0aecd72
RichardZhongQAQ <694834997@qq.com>
2019-05-04 12:52:49 +0800

    add RecipeDetailController class

---
+++ src/controller/RecipeDetailController.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class RecipeDetailController {
+    public RecipeDetailController(){
+
+    }
+    public void goBack(){
+        System.out.println("Go back!");
+    }
+}
