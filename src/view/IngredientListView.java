5583d9d325d0c9f90b5b3ec2ccd77055d8c5847b
dengrenjie <dengrenjie31@gmail.com>
2019-05-05 10:02:07 +0800

    Change ingredient list into note list's style

--- src/view/IngredientListView.java
+++ src/view/IngredientListView.java
@@ -57,16 +57,52 @@ public class IngredientListView extends View {
         });
         mainBody.add(JPanelSearchBar, BorderLayout.PAGE_START);
 
-        String[] columnNames = {"Ingredient", "Storage Amount", "Operation"};
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        for (int i = 0; i < 5; i++) {
+            JPanel mainPanelIter = new JPanel();
+            mainPanelIter.setLayout(new FlowLayout());
+            JLabel ingredientName = new JLabel("Barley");
+            mainPanelIter.add(ingredientName);
+            JLabel ingredientAmount = new JLabel("15g");
+            mainPanelIter.add(ingredientAmount);
+            JButton detailBtn = new JButton("detail");
+            JButton editBtn = new JButton("edit");
+            JButton deleteBtn = new JButton("delete");
+            detailBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //TODO: Show detail
+                }
+            });
+            editBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //TODO: Edit ingredient
+                }
+            });
+            deleteBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //TODO: Delete ingredient
+                }
+            });
+            mainPanelIter.add(detailBtn);
+            mainPanelIter.add(editBtn);
+            mainPanelIter.add(deleteBtn);
+            mainPanel.add(mainPanelIter);
+        }
+//        String[] columnNames = {"Ingredient", "Storage Amount", "Operation"};
+//
+//        Object[][] data =
+//                {
+//                        {"Barley", "1.5g", "detail edit delete"},
+//                        {"Yeast", "1.5g", "detail edit delete"},
+//                };
+//
+//        JTable table = new JTable(data, columnNames);
 
-        Object[][] data =
-                {
-                        {"Barley", "1.5g", "detail edit delete"},
-                        {"Yeast", "1.5g", "detail edit delete"},
-                };
-
-        JTable table = new JTable(data, columnNames);
-        mainBody.add(table, BorderLayout.CENTER);
+        mainBody.add(mainPanel, BorderLayout.CENTER);
         // TODO: Ingredient List
 
         this.add(mainBody, BorderLayout.CENTER);
