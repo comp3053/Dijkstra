297dc878145e295b02d1a21b4726adc63d50fc6c
RichardZhongQAQ <694834997@qq.com>
2019-05-05 12:12:29 +0800

    correct goBack method of RecipeDetailController class

--- src/controller/RecipeDetailController.java
+++ src/controller/RecipeDetailController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import view.RecipeListView;
+
 public class RecipeDetailController {
     public RecipeDetailController(){
 
     }
 
     public void goBack(){
-        System.out.println("Go back");
+        RecipeListController rlc = new RecipeListController();
+        RecipeListView rlv = new RecipeListView(rlc);
+        rlv.setVisible(true);
     }
 }
