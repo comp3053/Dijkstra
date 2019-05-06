75aefdf203e367c885adf7ef4c50f6d372faec13
dengrenjie <dengrenjie31@gmail.com>
2019-05-06 19:39:11 +0800

    Delete some useless things

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -3,6 +3,6 @@ package controller;
 import java.util.ArrayList;
 
 interface DatabaseController<T> {
-    public ArrayList<T> getAll() throws Exception;
-    public boolean insert(T t);
+    ArrayList<T> getAll() throws Exception;
+    boolean insert(T t);
 }
