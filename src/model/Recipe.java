2074454f99c48daa7830740cefe2763cb6d89d2e
RichardZhongQAQ <694834997@qq.com>
2019-05-06 22:58:19 +0800

    Modify amountConversion method in Recipe part because the Equipment unit change to ml

--- src/model/Recipe.java
+++ src/model/Recipe.java
@@ -63,14 +63,14 @@ public class Recipe {
         this.ingredients = ingredients;
     }
 
-    public void amountConversion(double originalBatchSize) throws InvalidInputException {//originalBatchSize should be used L as unit.This method is to convert all recipeIngredients to the 1L amount.
+    public void amountConversion(double originalBatchSize) throws InvalidInputException {//originalBatchSize should be used mL as unit.This method is to convert all recipeIngredients to the 1000mL amount.
         if(originalBatchSize<=0){
             throw new InvalidInputException("Batch size could not be equal or less than 0!");
         }
         else{
             for (RecipeIngredient ingredient : this.ingredients) {
                 try {
-                    ingredient.setAmount(ingredient.getAmount() / originalBatchSize);
+                    ingredient.setAmount(ingredient.getAmount() * originalBatchSize);
                 } catch (InvalidInputException e) {
                     e.printStackTrace();
                 }
