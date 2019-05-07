f8feb281163f09fc9f5f7bec4750cc368bd4990f
dengrenjie <dengrenjie31@gmail.com>
2019-05-07 22:48:17 +0800

    Add new DatabaseOperation interface and delete the old one

--- src/controller/BrewController.java
+++ src/controller/BrewController.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class BrewController implements DatabaseController<BrewingRecord> {
+public class BrewController {
     public BrewController() {
         //Nothing to do
     }
