57367a893cd6b7f196347c3f005ac686b3b0b0fc
LittleS <qqmsnco@126.com>
2019-05-08 21:12:20 +0800

    BrewingHistoryListView: modify

--- src/view/BrewingHistoryListView.java
+++ src/view/BrewingHistoryListView.java
@@ -1,15 +1,19 @@
 package view;
 
 import controller.BrewingHistoryListController;
+import model.BrewingRecord;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class BrewingHistoryListView extends View {
     private BrewingHistoryListController c;
+    private ArrayList<BrewingRecord> BrewingRecords;
     public BrewingHistoryListView(BrewingHistoryListController c){
+        this.BrewingRecords = c.readBrewingHistoryList();
         this.setTitle("Brew Day! - Brewing History"); // set frame title
         this.setSize(800, 600); // set frame size
         this.setLayout(new BorderLayout());
@@ -33,20 +37,21 @@ public class BrewingHistoryListView extends View {
 
         this.add(topLeftButtonBar, BorderLayout.NORTH);
 
+
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
-        for(int i =0;i<5;i++) {
+        for(BrewingRecord brewingRecord: BrewingRecords) {
             JPanel mainPanelIter = new JPanel();
             mainPanelIter.setLayout(new FlowLayout());
-            JLabel historyID = new JLabel("brew history" + " 03 ");
+            JLabel historyID = new JLabel("Brew History " + brewingRecord.getID());
             mainPanelIter.add(historyID);
-            JLabel timeText = new JLabel("2019-04-28 ");
+            JLabel timeText = new JLabel(String.valueOf(brewingRecord.getBrewDate()));
             mainPanelIter.add(timeText);
             JButton btn_takeNote = new JButton("Take note");
             btn_takeNote.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    c.takeNote();
+                    c.takeNote(brewingRecord);
                     dispose();
                 }
             });
