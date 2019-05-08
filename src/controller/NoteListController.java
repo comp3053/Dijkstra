626a6458ca90a182c6d7d7d62bc63394198dc14c
LittleS <qqmsnco@126.com>
2019-05-08 11:24:10 +0800

    NoteListController: modify

--- src/controller/NoteListController.java
+++ src/controller/NoteListController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import model.Note;
+import utils.FetchDataException;
 import view.BrewingHistoryListView;
 import view.HomeView;
+import view.NoteInputView;
+
+import javax.swing.*;
+import java.util.ArrayList;
 
 public class NoteListController {
     public NoteListController(){
-
     }
 
     public void goBack(){
@@ -14,18 +19,37 @@ public class NoteListController {
         hv.setVisible(true);
     }
 
-    public void readNoteList(){
-
+    public ArrayList<Note> readNoteList(){
+        ArrayList<Note> notes;
+        try {
+            notes = Note.getAllNotes();
+            return notes;
+        } catch (FetchDataException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public void modify(){
+    public void modify(Note m){
         System.out.println("modify");
-        // TODO, Fetch the id and modify the note
+        NoteInputController nc = new NoteInputController(m);
+        NoteInputView hv = new NoteInputView(nc,m);
+        hv.setVisible(true);
     }
 
-    public void delete(){
+    public void delete(Note m){
         System.out.println("delete");
-        // TODO, Fetch the id and do the deletion
+        int isSave = JOptionPane.showConfirmDialog(null,"Are you sure to delete your note?","Warning",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.WARNING_MESSAGE);
+        if (isSave == 0){
+            if(m.delete())
+                JOptionPane.showMessageDialog(null,"Your note has been deleted","Success",JOptionPane.PLAIN_MESSAGE);
+            else
+                JOptionPane.showMessageDialog(null,"Error!","Error",JOptionPane.ERROR_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(null,"Your note has not been deleted ","Success",JOptionPane.ERROR_MESSAGE);
+        }
+
+
     }
 
     public void addNote(){
