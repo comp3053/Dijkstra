e6ba3c645f786fb7ed2fd8362287486d161b82fa
LittleS <qqmsnco@126.com>
2019-05-08 10:47:27 +0800

    NoteListView: modify, now it support detail, delete and modify the notes

--- src/view/NoteListView.java
+++ src/view/NoteListView.java
@@ -2,20 +2,24 @@ package view;
 
 import controller.NoteContentController;
 import controller.NoteListController;
+import model.Note;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class NoteListView extends View {
     private NoteListController c;
+    private ArrayList<Note> notes;
+
     public NoteListView(NoteListController c){
         this.c = c;
+        this.notes = c.readNoteList();
         this.setTitle("Brew Day! - Note List"); // set frame title
         this.setSize(800, 600); // set frame size
         this.setLayout(new BorderLayout());
-
 
         JPanel topButtonsAround = new JPanel();
         topButtonsAround.setLayout(new BoxLayout(topButtonsAround, BoxLayout.LINE_AXIS));
@@ -51,20 +55,21 @@ public class NoteListView extends View {
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
-        for(int i =0;i<5;i++) {
+        for(Note note:notes) {
             JPanel mainPanelIter = new JPanel();
             mainPanelIter.setLayout(new FlowLayout());
-            JButton noteBtn = new JButton("brew history" + "03");
-            mainPanelIter.add(noteBtn);
-            JLabel forBrewingRecord = new JLabel("for brewing record");
+            JLabel brewHistory = new JLabel("Brew History " + note.getID());
+            mainPanelIter.add(brewHistory);
+            JLabel forBrewingRecord = new JLabel("for Brewing History " + note.getBrewID());
             mainPanelIter.add(forBrewingRecord);
+            JButton detailBtn = new JButton("detail");
             JButton modifyBtn = new JButton("modify");
             JButton deleteBtn = new JButton("delete");
-            noteBtn.addActionListener(new ActionListener() {
+            detailBtn.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     NoteContentController ncc = new NoteContentController();
-                    NoteContentView ncv = new NoteContentView(ncc, 1);
+                    NoteContentView ncv = new NoteContentView(ncc,note);
                     // TODO: Need to change according to click (better pass in a model)
                     ncv.setVisible(true);
                     dispose();
@@ -73,17 +78,17 @@ public class NoteListView extends View {
             deleteBtn.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    c.delete();
+                    c.delete(note);
                 }
             });
             modifyBtn.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-//                    TODO: Modify Note
-//                    c.modify();
-//                    dispose();
+                    c.modify(note);
+                    dispose();
                 }
             });
+            mainPanelIter.add(detailBtn);
             mainPanelIter.add(modifyBtn);
             mainPanelIter.add(deleteBtn);
             mainPanel.add(mainPanelIter);
