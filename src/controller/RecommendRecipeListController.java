0f64184386e0229246d66f30ba6cfce8decce27c
ecwu <bitlabwzh@gmail.com>
2019-05-11 23:47:57 +0800

    shorter the code

--- src/controller/RecommendRecipeListController.java
+++ src/controller/RecommendRecipeListController.java
@@ -12,55 +12,50 @@ import java.util.ArrayList;
 
 public class RecommendRecipeListController {
     private ArrayList<Recipe> recommendRecipes;
-    public RecommendRecipeListController(ArrayList<Recipe> recommendRecipes){
-        this.recommendRecipes= recommendRecipes;
+
+    public RecommendRecipeListController(ArrayList<Recipe> recommendRecipes) {
+        this.recommendRecipes = recommendRecipes;
     }
 
-    public void goBack(){
+    public void goBack() {
         HomeController hc = new HomeController();
         HomeView hv = new HomeView(hc);
         hv.setVisible(true);
     }
 
-    public void brewRecipe(ArrayList<Recipe> recommendRecipe, int recipeID){
-        for(Recipe recipe: recommendRecipe){
-            if (recipe.getID() == recipeID){
-                try {
-                    int equipmentBatchSize=Equipment.getEquipment(1).getVolume();
-                    recipe.amountConversion(1000,equipmentBatchSize);
-                } catch (FetchDataException | InvalidInputException | EmptyNameException e) {
-                    e.printStackTrace();
-                }
-                BrewDetailController bdc = new BrewDetailController(recipe);
-                BrewDetailView bdv = new BrewDetailView(bdc,recipe);
-                recipe.addListener(bdv);
-                bdv.setVisible(true);
-                break;
-            }
+    public void brewRecipe(Recipe recommendRecipe) {
+        try {
+            int equipmentBatchSize = Equipment.getEquipment(1).getVolume();
+            recommendRecipe.amountConversion(1000, equipmentBatchSize);
+        } catch (FetchDataException | InvalidInputException | EmptyNameException e) {
+            e.printStackTrace();
         }
-
+        BrewDetailController bdc = new BrewDetailController(recommendRecipe);
+        BrewDetailView bdv = new BrewDetailView(bdc, recommendRecipe);
+        recommendRecipe.addListener(bdv);
+        bdv.setVisible(true);
     }
 
-    public void generateShoppingList(ArrayList<Recipe> recommendRecipe, int recipeID){
+    public void generateShoppingList(Recipe notEnoughRecommendRecipe) {
         try {
-            Recipe recipe = recommendRecipe.get(recipeID-1);
             ArrayList<RecipeIngredient> recipeIngredients = new ArrayList<>();
             ArrayList<StorageIngredient> storageIngredients = StorageIngredient.getAll();
-            for(int i =0;i<recipe.getIngredients().size();i++) {
+            int equipment = Equipment.getEquipment(1).getVolume();
+            for (RecipeIngredient recipeIngredientItem : notEnoughRecommendRecipe.getIngredients()) {
                 for (StorageIngredient storageIngredient : storageIngredients) {
-                    if (storageIngredient.getID() == recipe.getIngredients().get(i).getID()) {
-                        if (storageIngredient.getAmount() < recipe.getIngredients().get(i).getAmount()) {
-                            recipe.getIngredients().get(i).setAmount(recipe.getIngredients().get(i).getAmount() - storageIngredient.getAmount());
-                            recipeIngredients.add(recipe.getIngredients().get(i));
+                    if (storageIngredient.getID() == recipeIngredientItem.getID()) {
+                        if (storageIngredient.getAmount() < recipeIngredientItem.getAmount()) {
+                            recipeIngredientItem.setAmount(recipeIngredientItem.getAmount() / 1000 * equipment - storageIngredient.getAmount());
+                            recipeIngredients.add(recipeIngredientItem);
                         }
                     }
                 }
             }
-            recipe.setIngredients(recipeIngredients);
+            notEnoughRecommendRecipe.setIngredients(recipeIngredients);
             MissingIngredientListController milc = new MissingIngredientListController();
-            MissingIngredientsListView milv = new MissingIngredientsListView(milc,recipe);
+            MissingIngredientsListView milv = new MissingIngredientsListView(milc, notEnoughRecommendRecipe);
             milv.setVisible(true);
-        } catch (FetchDataException | InvalidInputException e) {
+        } catch (FetchDataException | EmptyNameException | InvalidInputException e) {
             e.printStackTrace();
         }
     }
