a7089b987a91b73b809fb1d4803d305ec5205458
RichardZhongQAQ <694834997@qq.com>
2019-05-17 19:54:55 +0800

    add some comment in MissingIngredientListController and Controller class

--- src/controller/MissingIngredientListController.java
+++ src/controller/MissingIngredientListController.java
@@ -10,12 +10,12 @@ public class MissingIngredientListController {
         // Nothing to do
     }
 
-    public void goBack() {
+    public void goBack() {// go back to the recommend list view
         HomeController hc = new HomeController();
         hc.startRecommend();
     }
 
-    public void OK() {
+    public void OK() {// go back to the home view
         HomeController hc = new HomeController();
         HomeView hv = new HomeView(hc);
         hv.setVisible(true);
