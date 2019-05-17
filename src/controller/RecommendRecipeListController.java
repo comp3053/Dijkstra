962632fdf8984b2703ea2909f3761ab192553f0f
RichardZhongQAQ <694834997@qq.com>
2019-05-17 18:59:34 +0800

    add some comment for RecommendRecipeList class.

--- src/controller/RecommendRecipeListController.java
+++ src/controller/RecommendRecipeListController.java
@@ -13,17 +13,17 @@ import java.util.ArrayList;
 public class RecommendRecipeListController {
     private ArrayList<Recipe> recommendRecipes;
 
-    public RecommendRecipeListController(ArrayList<Recipe> recommendRecipes) {
+    public RecommendRecipeListController(ArrayList<Recipe> recommendRecipes) {//initial the recommend recipe list controller
         this.recommendRecipes = recommendRecipes;
     }
 
-    public void goBack() {
+    public void goBack() {// go back to the home view
         HomeController hc = new HomeController();
         HomeView hv = new HomeView(hc);
         hv.setVisible(true);
     }
 
-    public void brewRecipe(Recipe recommendRecipe,ArrayList<Recipe> recommendRecipes) {
+    public void brewRecipe(Recipe recommendRecipe,ArrayList<Recipe> recommendRecipes) {// go to the recommend recipe view
         try {
             int equipmentBatchSize = Equipment.getEquipment(1).getVolume();
             recommendRecipe.amountConversion(1000, equipmentBatchSize);
@@ -32,15 +32,17 @@ public class RecommendRecipeListController {
         }
         BrewDetailController bdc = new BrewDetailController(recommendRecipe);
         BrewDetailView bdv = new BrewDetailView(bdc, recommendRecipe,recommendRecipes);
+        // add a listener for update the brew detail view
         recommendRecipe.addListener(bdv);
         bdv.setVisible(true);
     }
 
-    public void generateShoppingList(Recipe notEnoughRecommendRecipe,ArrayList<Recipe> recommendRecipes) {
+    public void generateShoppingList(Recipe notEnoughRecommendRecipe,ArrayList<Recipe> recommendRecipes) {//go to the generate shopping list view
         try {
             ArrayList<RecipeIngredient> recipeIngredients = new ArrayList<>();
             ArrayList<StorageIngredient> storageIngredients = StorageIngredient.getAll();
             int equipment = Equipment.getEquipment(1).getVolume();
+            // for loop is used to get the required ingredient and calculate the required amount
             for (RecipeIngredient recipeIngredientItem : notEnoughRecommendRecipe.getIngredients()) {
                 for (StorageIngredient storageIngredient : storageIngredients) {
                     if (storageIngredient.getID() == recipeIngredientItem.getID()) {
