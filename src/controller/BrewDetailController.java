1c25b45ab87b2d2399de3e0d0b16db766710cbfc
RichardZhongQAQ <694834997@qq.com>
2019-05-17 19:14:09 +0800

    add some comment in BrewDetailController class

--- src/controller/BrewDetailController.java
+++ src/controller/BrewDetailController.java
@@ -11,16 +11,16 @@ import java.util.Date;
 public class BrewDetailController {
     private Recipe m;
 
-    public BrewDetailController(Recipe m) {
+    public BrewDetailController(Recipe m) {//initial the brew detail controller
         this.m = m;
     }
 
-    public void goBack() {
+    public void goBack() {// go back to the recommend recipe list page
         HomeController hc = new HomeController();
         hc.startRecommend();
     }
 
-    public void applyBatchSize(int originBatchSize, int currentBatchSize) {
+    public void applyBatchSize(int originBatchSize, int currentBatchSize) {// change the ingredient batch size in the table
         try {
             m.amountConversion(originBatchSize, currentBatchSize);
         } catch (InvalidInputException e) {
@@ -28,7 +28,7 @@ public class BrewDetailController {
         }
     }
 
-    public void brewRecipe(int batchSize) {
+    public void brewRecipe(int batchSize) {// go to brew recipe page and also finish one brew(cost ingredient and add a brew history)
         BrewRecipeController brc = new BrewRecipeController();
         BrewingRecord br = new BrewingRecord(new Date(), batchSize, m);
         br.insert();
