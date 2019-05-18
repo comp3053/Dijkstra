95c0febf2be38d6783877c39d9be45a782ddb38c
dengrenjie <dengrenjie31@gmail.com>
2019-05-18 12:29:53 +0800

    MissingIngredientController: Add comments

--- src/controller/MissingIngredientListController.java
+++ src/controller/MissingIngredientListController.java
@@ -1,21 +1,24 @@
 package controller;
 
-import model.Recipe;
 import view.HomeView;
-
-import java.util.ArrayList;
 
 public class MissingIngredientListController {
     public MissingIngredientListController() {
         // Nothing to do
     }
 
-    public void goBack() {// go back to the recommend list view
+    /**
+     * Go back to recommend list view.
+     */
+    public void goBack() {
         HomeController hc = new HomeController();
         hc.startRecommend();
     }
 
-    public void OK() {// go back to the home view
+    /**
+     * Go back to homepage.
+     */
+    public void OK() {
         HomeController hc = new HomeController();
         HomeView hv = new HomeView(hc);
         hv.setVisible(true);
