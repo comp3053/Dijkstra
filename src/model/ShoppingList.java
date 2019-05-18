070f1efa1e9b165e4eccc838856a5d6dfa0c3bec
dengrenjie <dengrenjie31@gmail.com>
2019-05-18 10:08:41 +0800

    ShoppingList: Update comment

--- src/model/ShoppingList.java
+++ src/model/ShoppingList.java
@@ -13,10 +13,20 @@ public class ShoppingList {
         return this.missingIngredients;
     }
 
+    /**
+     * Calculate the amount of missing ingredients you need to buy.
+     * @param restAmount Rest amount of storage ingredient in database
+     * @param requireAmount Amount of ingredient you need for a brew.
+     * @return The amount of ingredient you need to buy.
+     */
     public double calculateMissingIngredientAmount(double restAmount, double requireAmount) {
         return (requireAmount - restAmount);
     }
 
+    /**
+     * Add missing ingredient into Shopping list.
+     * @param missingIngredients Ingredient you need to buy
+     */
     public void addMissingIngredient(Ingredient missingIngredients) {
         this.missingIngredients.add(missingIngredients);
     }
