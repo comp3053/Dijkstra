87b27e1a00d93dd21c89386aca9a2c40ce8e167a
dengrenjie <dengrenjie31@gmail.com>
2019-05-18 12:39:39 +0800

    EquipmentInfoController: Add comments

--- src/controller/EquipmentInfoController.java
+++ src/controller/EquipmentInfoController.java
@@ -11,12 +11,18 @@ public class EquipmentInfoController {
         this.m = m;
     }
 
+    /**
+     * Go back to home page.
+     */
     public void goBack() {
         HomeController hc = new HomeController();
         HomeView hv = new HomeView(hc);
         hv.setVisible(true);
     }
 
+    /**
+     * Go to update equipment page.
+     */
     public void turnUpdateInfo() {
         UpdateEquipmentInfoController ueic = new UpdateEquipmentInfoController(m);
         UpdateEquipmentInfoView ueiv = new UpdateEquipmentInfoView(ueic, m, false);
