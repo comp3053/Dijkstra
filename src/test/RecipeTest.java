5164e389d24a69919b134ed5563507b93b5facde
Richard <694834997@qq.com>
2019-05-20 17:46:25 +0800

    add comment for RecipeTest class

--- src/test/RecipeTest.java
+++ src/test/RecipeTest.java
@@ -10,62 +10,62 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class RecipeTest {
-
+    // initial part
     private static Recipe recipe;
     ArrayList<RecipeIngredient> recipeIngredients = new ArrayList<>();
 
     @Before
-    public void setUp() {
+    public void setUp() {// set up the recipe
         recipe = new Recipe(1,"beer","good",recipeIngredients);
     }
 
     @Test
-    public void getID() {
+    public void getID() {// test whether it can get ID
         assertEquals(1,recipe.getID());
     }
 
     @Test
-    public void setID() {
+    public void setID() {// test whether it can set ID
         recipe.setID(2);
         assertEquals(2,recipe.getID());
     }
 
     @Test
-    public void getName() {
+    public void getName() {// test whether it can get name
         assertEquals("beer",recipe.getName());
     }
 
     @Test
-    public void setName() {
+    public void setName() {// test whether it can set name
         recipe.setName("badBeer");
         assertEquals("badBeer",recipe.getName());
     }
 
     @Test
-    public void getDescription() {
+    public void getDescription() {// test whether it get description
         assertEquals("good",recipe.getDescription());
     }
 
     @Test
-    public void setDescription() {
+    public void setDescription() {// test whether it can set description
         recipe.setDescription("bad");
         assertEquals("bad",recipe.getDescription());
     }
 
     @Test
-    public void getIngredients() {
+    public void getIngredients() {// test whether it can get ingredients
         assertEquals(recipeIngredients,recipe.getIngredients());
     }
 
     @Test
-    public void setIngredients() {
+    public void setIngredients() {// test whether it can set ingredients
         ArrayList<RecipeIngredient> modifyRecipeIngredient = new ArrayList<>();
         recipe.setIngredients(modifyRecipeIngredient);
         assertEquals(modifyRecipeIngredient,recipe.getIngredients());
     }
 
     @Test
-    public void addRecipeIngredient() {
+    public void addRecipeIngredient() {// test whether it can add recipe ingredient
         try {
             RecipeIngredient water = new RecipeIngredient(1,"water",100.0, UnitEnum.LITER);
             try {
@@ -80,7 +80,7 @@ public class RecipeTest {
     }
 
     @Test
-    public void amountConversion() {
+    public void amountConversion() {// test whether it can do amount conversion
         try {
             RecipeIngredient water = new RecipeIngredient(1,"water",100.0, UnitEnum.LITER);
             recipe.addRecipeIngredient(water);
@@ -92,7 +92,7 @@ public class RecipeTest {
     }
 
     @Test
-    public void modifyRecipeIngredient() {
+    public void modifyRecipeIngredient() {// test whether it can modify recipe ingredient
         try {
             RecipeIngredient water = new RecipeIngredient(1,"water",50,UnitEnum.LITER);
             RecipeIngredient modifyWater = new RecipeIngredient(2,"water",50,UnitEnum.LITER);
