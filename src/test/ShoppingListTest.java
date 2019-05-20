44e88445cb2bc86821b135d7b04505a470ee8791
Richard <694834997@qq.com>
2019-05-20 17:48:30 +0800

    add comment for ShoppingListTest class

--- src/test/ShoppingListTest.java
+++ src/test/ShoppingListTest.java
@@ -13,27 +13,27 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class ShoppingListTest {
-
+    //initial part
     private static ShoppingList shoppingList;
     ArrayList<Ingredient> missingIngredients = new ArrayList<>();
 
     @Before
-    public void setUp() {
+    public void setUp() {// set up the shopping list
         shoppingList = new ShoppingList(missingIngredients);
     }
 
     @Test
-    public void calculateMissingIngredientAmount() {
+    public void calculateMissingIngredientAmount() {// test whether it can calculate missing ingredient amount
         assertEquals(2.0,shoppingList.calculateMissingIngredientAmount(3.0,5.0),0.01);
     }
 
     @Test
-    public void getMissingIngredients(){
+    public void getMissingIngredients(){// test whether it can get missing ingredient
         assertEquals(missingIngredients,shoppingList.getMissingIngredients());
     }
 
     @Test
-    public void addMissingIngredient() {
+    public void addMissingIngredient() {// test whether it can add missing ingredient
         Ingredient ingredient;
         try {
             ingredient = new Ingredient(1, "yeast", 100.0, UnitEnum.GRAM);
